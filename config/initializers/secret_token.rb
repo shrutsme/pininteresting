f72a3012b7358c826285a3e7a7a70fab51563a02
Shruti Gautam <shruti.gautam.dce@gmail.com>
2013-12-31 13:14:11 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Piniteresting::Application.config.secret_key_base = '7c91373ada6b883f6c2f055cfa6c0d4f80e5d688644d93c75a26c30168c45f1af05b447e167e885bb7a812d40f8b61bb6390a499e7eadda14cb8e2a558e08b86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Piniteresting::Application.config.session_store :cookie_store, key: '_piniteresting_session'
