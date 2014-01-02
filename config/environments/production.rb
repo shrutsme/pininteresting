28b123ad4499b0d2f94f09bbe4e1c26c3522d54a
Shruti Gautam <shruti.gautam.dce@gmail.com>
2014-01-01 20:37:30 -0800

    Implemented for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,8 @@ Piniteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  # Required for heroku
+  # Note to set this
+  config.action_mailer.default_url_options = { :host => 'shr-pininteresting.herokuapp.com' }
 end
