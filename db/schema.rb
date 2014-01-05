175f5a03b3437651c3d891007ceab8a6c1d4bfcc
Shruti Gautam <shruti.gautam.dce@gmail.com>
2014-01-04 18:35:45 -0800

    Add image upload with paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140104231946) do
+ActiveRecord::Schema.define(version: 20140105011228) do
 
 # Could not dump table "pins" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
