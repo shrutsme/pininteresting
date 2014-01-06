bdafe7c323d24329d040215914029a1b9227a280
Shruti Gautam <shruti.gautam.dce@gmail.com>
2014-01-05 17:09:49 -0800

    Added JQuery masonry

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,14 @@ Piniteresting::Application.configure do
   # Required for heroku
   # Note to set this
   config.action_mailer.default_url_options = { :host => 'shr-pininteresting.herokuapp.com' }
+ 
+  #set Paperclip to upload images to S3
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
